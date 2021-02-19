25bcad1ec74e27b55b13e1c773e029f180f8ca1e
David Hacker <dmhacker@protonmail.com>
2021-02-18 17:09:24 -0800

    Fix stats & subproject names

--- backend/services/stats.js
+++ backend/services/stats.js
@@ -4,14 +4,25 @@ const config = require('../config');
 const { Application, Review, UserCategory } = require('../models');
 const { ServiceError } = require('./errors')
 
-//Expecting type Date
+/**
+ * Returns statistics related to the number of applications
+ * between two dates. The dates refer to when the application
+ * was created, not necessarily updated.
+ */
 async function getApplicationStats(startDate, endDate) {
   const stats = {};
+  if (startDate == null) {
+    startDate = new Date(628021800000); // The epoch: around 1970
+  }
+  if (endDate == null) {
+    endDate = new Date();
+  }
   for (const role of config.roles) {
     stats[role] = {};
     for (const stage of config.stages) {
-      stats[role][stage] = await Application.count({
+      stats[role][stage] = await Application.countDocuments({
         role: role,
+        completed: false,
         current_stage: stage,
         created_at: {
           $gte: startDate,
@@ -19,7 +30,7 @@ async function getApplicationStats(startDate, endDate) {
         }
       }).exec();
     }
-    stats[role]['ACCEPTED'] = await Application.count({
+    stats[role]['ACCEPTED'] = await Application.countDocuments({
       role: role,
       completed: true,
       accepted: true,
@@ -28,7 +39,7 @@ async function getApplicationStats(startDate, endDate) {
         $lte: endDate
       }
     }).exec();
-    stats[role]['REJECTED'] = await Application.count({
+    stats[role]['REJECTED'] = await Application.countDocuments({
       role: role,
       completed: true,
       accepted: false,
