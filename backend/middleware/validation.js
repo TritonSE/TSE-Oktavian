8ef0d8e2b6c3a09c6882dda6a2cccc8d16b9963e
David Hacker <dmhacker@protonmail.com>
2021-02-27 11:19:59 -0800

    Improve backend error handling - correct JSON is returned no matter what + comments added

--- backend/middleware/validation.js
+++ backend/middleware/validation.js
@@ -1,11 +1,23 @@
 const { validationResult } = require("express-validator");
 
+/**
+ * After specifying a list of validation rules for some route taking in
+ * input, use this middleware to trigger all of the validation/sanitization
+ * rules. The request will be rejected with a 400 with it does not meet
+ * the validation criteria.
+ */
 const validateRequest = (req, res, next) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
+  const vres = validationResult(req);
+  if (vres.isEmpty()) {
     return next();
   } else {
-    return res.status(400).json({ message: "User input is malformed" });
+    const bad_fields = new Set();
+    for (const error of vres.errors) {
+      bad_fields.add(error.param);
+    }
+    return res
+      .status(400)
+      .json({ message: `Invalid or missing: ${[...bad_fields].join(", ")}` });
   }
 };
 
