cc0e45a3373c29f3ef9d3c9dbfad0de5f10e9ba6
David Hacker <dmhacker@protonmail.com>
2021-04-08 12:01:35 -0400

    Update default MONGO_URI with database name

--- backend/constants/index.js
+++ backend/constants/index.js
@@ -5,7 +5,7 @@ module.exports = {
   PORT: process.env.PORT || "8000",
   REGISTER_SECRET: process.env.REGISTER_SECRET || "tritonse",
   JWT_SECRET: process.env.JWT_SECRET || "keyboard cat",
-  MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017",
+  MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/tse-oktavian",
   EMAIL_USERNAME: process.env.EMAIL_USERNAME || "",
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || "",
   STAGES: ["Resume", "Phone", "Interview", "Final"],
