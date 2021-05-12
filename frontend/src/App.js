8eb162cbed683eab166291b55181f88cad3eedd5
zainnk902 <68146410+zainnk902@users.noreply.github.com>
2021-05-11 22:13:19 -0700

    Added final page to show relevant information for each final application (#41)
    * Added final pag to view relevant info for all Final Applications
    
    * Added final page to show relevant ifornation for each final application
    
    * fixed changes on Final page
    
    * Fixed final applications page issues
    
    * Fixed resume issue
    
    * fixed parameter issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import Applications from "./pages/recruitment/Applications";
 import Assignments from "./pages/recruitment/Assignments";
 import Application from "./pages/recruitment/Application";
 import NewApplication from "./pages/recruitment/NewApplication";
+import Final from "./pages/recruitment/Final";
 import Pipelines from "./pages/recruitment/pipeline/Pipelines";
 import Roster from "./pages/Roster";
 import RosterInfo from "./pages/RosterInfo";
@@ -55,6 +56,9 @@ export default function App() {
           </Route>
           <Route exact path="/recruitment/applications">
             <Applications />
+          </Route>
+          <Route exact path="/recruitment/final">
+            <Final />
           </Route>
           <Route
             path="/recruitment/application/:appid"
