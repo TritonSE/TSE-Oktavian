ec6e4bb0522500aac39741e0949fa6ffe8eef4a9
Philip Zhang <jmslca123@gmail.com>
2021-05-30 12:36:47 -0700

    add external recruitment checkbox

--- frontend/src/components/RolePermissionsCard.js
+++ frontend/src/components/RolePermissionsCard.js
@@ -143,8 +143,15 @@ const RolePermissionCard = ({ role, mode }) => {
 
   const handleChange = (event) => {
     const { permissions } = state.role;
+    let { external_recruitment } = state.role;
     const field_name = event.target.name;
-    permissions[field_name] = event.target.checked;
+    if (field_name === "external_recruitment") {
+      // handle special case for external_recruitment field
+      external_recruitment = event.target.checked;
+    } else {
+      permissions[field_name] = event.target.checked;
+    }
+
     // unchecks admin box if any indented checkboxes were unchecked
     if (adminSubPermissions.includes(field_name) && !permissions[field_name]) {
       permissions["admin"] = false;
@@ -155,6 +162,7 @@ const RolePermissionCard = ({ role, mode }) => {
       role: {
         ...prevState.role,
         permissions,
+        external_recruitment,
       },
     }));
   };
@@ -313,6 +321,16 @@ const RolePermissionCard = ({ role, mode }) => {
                   />
                 }
                 label="Activate/Deactivate Accounts"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={state.role.external_recruitment}
+                    onChange={handleChange}
+                    name="external_recruitment"
+                  />
+                }
+                label="Can be assigned to pending users"
               />
             </FormGroup>
           </FormControl>
